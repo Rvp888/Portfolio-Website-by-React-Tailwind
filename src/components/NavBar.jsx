eef6deb664b869a619e261afe0aa6f32e333f1bc
Rohan Palankar <rohanpalankar8@gmail.com>
2023-08-05 17:51:31 +0530

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const NavBar = () => {
+  const links = [
+    {
+      id: 1,
+      link: "home",
+    },
+    {
+      id: 2,
+      link: "about",
+    },
+    {
+      id: 3,
+      link: "portfolio",
+    },
+    {
+      id: 4,
+      link: "experience",
+    },
+    {
+      id: 5,
+      link: "contact",
+    },
+  ];
+
+  return (
+    <div className="flex justify-between items-center w-full px-4 h-20 bg-black text-white fixed">
+      <div>
+        <h1 className="text-5xl font-signature ml-2">Rohan</h1>
+      </div>
+
+      <ul className="hidden md:flex">
+        {links.map(({ id, link }) => (
+          <li
+            key={id}
+            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+          >
+            {link}
+          </li>
+        ))}
+      </ul>
+
+      <div>
+        <FaBars size={30} />
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
