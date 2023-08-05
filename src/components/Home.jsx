c6d3b088ac388a5339b394d99c63f482bec7db7f
Rohan Palankar <rohanpalankar8@gmail.com>
2023-08-05 20:40:25 +0530

    social links added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     >
       <div className="max-w-screen-lg mx-auto flex flex-col justify-center items-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+          <h2 className="text-4xl md:text-7xl font-bold text-white">
             I'm a Front End Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
@@ -23,7 +23,7 @@ const Home = () => {
             <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
-                <MdOutlineKeyboardArrowRight />
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1"/>
               </span>
             </button>
           </div>
@@ -31,7 +31,7 @@ const Home = () => {
 
         <div>
           <img
-            className="w-1/2 md:w-96 mx-auto rounded-2xl"
+            className="w-56 md:w-96 mx-auto rounded-2xl"
             src={HeroImg}
             alt="my profile"
           />
