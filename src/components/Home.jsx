efe3b6be953f50ce5c7e8032c0e32ac21acab895
Rohan Palankar <rohanpalankar8@gmail.com>
2023-08-05 19:04:26 +0530

    Home component updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,11 +4,11 @@ import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
 
 const Home = () => {
   return (
-    <div>
-      <div>
-        <div>
-            <h2>I'm a Front End Developer</h2>
-            <p>
+    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
+      <div className='max-w-screen-lg mx-auto flex flex-col justify-center items-center h-full px-4 md:flex-row'>
+        <div className='flex flex-col justify-center h-full'>
+            <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Front End Developer</h2>
+            <p className='text-gray-500 py-4 max-w-md'>
                 I have more than a year of experience building softwares.
                 Currently, I love to work on web application using technologies like
                 React, Tailwind and Bootstrap.
@@ -22,6 +22,10 @@ const Home = () => {
                     </span>
                 </button>
             </div>
+        </div>
+
+        <div>
+            <img className='w-1/2 md:w-96 mx-auto rounded-2xl' src={HeroImg} alt="my profile" />
         </div>
       </div>
     </div>
