daff5bc496a1e5eb4f080ce35a8c1e28c71daa32
Rohan Palankar <rohanpalankar8@gmail.com>
2023-08-06 10:00:16 +0530

    about section created

---
+++ src/components/About.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+      
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+        </div>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+          suscipit a velit, eligendi vel unde eius. Id eius sequi voluptatum,
+          itaque necessitatibus, harum minus cupiditate aspernatur odio atque
+          error. Qui, iusto. Numquam, cumque eos provident, quia obcaecati
+          labore earum dolore ab harum consequatur, praesentium necessitatibus.
+          Eos reiciendis distinctio perspiciatis exercitationem!
+        </p>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio ex
+          saepe dolor deserunt esse. Facere, aliquid. Magni eaque placeat,
+          repellendus tenetur molestias nesciunt voluptate facere. Temporibus ex
+          omnis neque velit doloremque exercitationem, quos nesciunt dicta nisi
+          expedita perspiciatis quo harum suscipit tenetur magni animi amet
+          delectus nihil. Dolorem, pariatur hic.
+        </p>
+      </div>
+
+    </div>
+  );
+};
+
+export default About;
