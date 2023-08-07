cfa2aa7c0de2d59e119f2ce2c441d9694a2f7767
Rohan Palankar <rohanpalankar8@gmail.com>
2023-08-07 17:39:18 +0530

    about section updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 const About = () => {
   return (
-    <div name="about" className="w-full md:h-screen bg-gradient-to-b from-gray-800 to-black text-white pt-16 px-4">
-      
+    <div
+      name="about"
+      className="w-full md:h-screen bg-gradient-to-b from-gray-800 to-black text-white pt-16 px-4"
+    >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div>
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
         </div>
 
         <p className="text-xl mt-10 ">
@@ -19,15 +23,13 @@ const About = () => {
         </p>
         <br />
         <p className="text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio ex
-          saepe dolor deserunt esse. Facere, aliquid. Magni eaque placeat,
-          repellendus tenetur molestias nesciunt voluptate facere. Temporibus ex
-          omnis neque velit doloremque exercitationem, quos nesciunt dicta nisi
-          expedita perspiciatis quo harum suscipit tenetur magni animi amet
-          delectus nihil. Dolorem, pariatur hic.
+          I have been coding from last one year and have developed a strong
+          skillset in HTML, CSS, and JavaScript. I am also proficient in React.Js. 
+          I have a good understanding of how to create responsive
+          and user-friendly webpages that are both visually appealing and easy
+          to use.
         </p>
       </div>
-
     </div>
   );
 };
