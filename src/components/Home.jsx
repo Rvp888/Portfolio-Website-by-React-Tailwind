1a3520927295d2e9427c48c97c16a36c0898fb1f
Rohan Palankar <rohanpalankar8@gmail.com>
2023-08-21 10:46:46 +0530

    home section updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import HeroImg from "../assets/heroImage.jpg";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
-import { Link } from 'react-scroll';
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
@@ -17,14 +17,19 @@ const Home = () => {
           <p className="text-gray-500 py-4 max-w-md">
             I have more than a year of experience building softwares. Currently,
             I love to work on web application using technologies like React,
-            Tailwind and Bootstrap.
+            jQuery and Tailwind CSS.
           </p>
 
           <div>
-            <Link to="portfolio" smooth duration={500} className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
-                <MdOutlineKeyboardArrowRight size={25} className="ml-1"/>
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
             </Link>
           </div>
